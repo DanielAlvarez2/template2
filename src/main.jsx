6f326a0b7b93874e77b837e6036ae27269be5fd4
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-11 17:14:06 -0400

    bug fix: declare routes inside of Strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.jsx'
+import Home from './components/Home.jsx'
+import Allergies from './components/Allergies.jsx'
 import Client from './Client.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Client />
+    <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/app' element={<App />} />
+                    <Route path='/allergies' element={<Allergies />} />
+                </Routes>        
+    </BrowserRouter>
   </StrictMode>,
 )
