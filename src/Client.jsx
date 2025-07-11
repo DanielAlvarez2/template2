6def50399c7518e90ed0719a8315e5a5b9a6f3c8
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-11 14:08:11 -0400

    create /allergies page and component and route

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Home from './components/Home.jsx'
 import App from './components/App.jsx'
+import Allergies from './components/Allergies.jsx'
 
 export default function Client(){
     return (
@@ -9,6 +10,7 @@ export default function Client(){
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/app' element={<App />} />
+                    <Route path='/allergies' element={<Allergies />} />
                 </Routes>
             
             </BrowserRouter>
---
+++ src/components/Allergies.jsx
@@ -0,0 +1,5 @@
+export default function Allergies(){
+    return (
+        <h1 style={{width:'100%'}}>Allergies Page</h1>
+    )
+}
