092abac05cf73c3219730ef723deddd1923f0ff0
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-18 11:19:57 -0400

    commemnt out app.get(*)

--- server/server.js
+++ server/server.js
@@ -216,13 +216,14 @@ app.put('/api/whitespace/vertical', async(req,res)=>{
     }
 })
 
-console.log('NODE: '+process.env.NODE_ENV)
+console.log('NODE_ENV: '+process.env.NODE_ENV)
 console.log(path)
+console.log('PATH.DIRNAME: '+path.dirname)
 
 
-if (process.env.NODE_ENV == 'production'){
-    app.get('*',(req,res)=>res.sendFile('../dist/index.html'))
-}
+// if (process.env.NODE_ENV == 'production'){
+//     app.get('*',(req,res)=>res.sendFile('../dist/index.html'))
+// }
 
 const PORT = process.env.PORT || 9991
 app.listen(PORT, ()=> console.log(`Server Running on Port: ${PORT}`))
