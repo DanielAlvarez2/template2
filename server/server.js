d673713ed1ef72b17129b02f9437ea88d28cc1da
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-18 03:30:03 -0400

    typo: 1 too many )

--- server/server.js
+++ server/server.js
@@ -219,7 +219,7 @@ app.put('/api/whitespace/vertical', async(req,res)=>{
 console.log('NODE: '+process.env.NODE_ENV)
 
 if (process.env.NODE_ENV == 'production'){
-    app.get('*',(req,res)=>res.sendFile('../dist/index.html')))
+    app.get('*',(req,res)=>res.sendFile('../dist/index.html'))
 }
 
 const PORT = process.env.PORT || 9991
