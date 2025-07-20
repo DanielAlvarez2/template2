0ff0d273c7ffd89ff6ce4e1a2e0be2c31cf697f6
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-20 01:51:27 -0400

    debugging: react-router

--- server/server.js
+++ server/server.js
@@ -221,10 +221,9 @@ console.log(path)
 console.log('PATH.DIRNAME: '+path.dirname)
 console.log('PATH.RESOLVE(): '+path.resolve())
 
-
-// if (process.env.NODE_ENV == 'production'){
-//     app.get('*',(req,res)=>res.sendFile(path.join(path.resolve(),'index.html')))
-// }
+if (process.env.NODE_ENV == 'production'){
+    app.get('*',(req,res)=>res.sendFile('index.html'))
+}
 
 const PORT = process.env.PORT || 9991
 app.listen(PORT, ()=> console.log(`Server Running on Port: ${PORT}`))
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { BrowserRouter,Routes,Route } from "react-router"
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.jsx'
 import Home from './components/Home.jsx'
@@ -13,8 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/allergies' element={<Allergies />} />
+        <Route path='home' element={<Home />} />
+        <Route path='allergies' element={<Allergies />} />
       </Routes>
     </BrowserRouter>
   
